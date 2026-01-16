1d60ad90aa0c85d743b5176b0e17240f72ceb6fc
Arya Shedbal <moony.shady.o7@gmail.com>
2026-01-16 12:47:15 +0530

    corrected syntax and logic

--- Arya/fc.v
+++ Arya/fc.v
@@ -11,6 +11,7 @@ module fc
   reg signed [15:0] weights [0:5][0:3];
   reg signed [31:0] acc;
   reg [2:0] in_idx, out_idx;
+  integer i, j;
   
   parameter idle=3'b000, init_out=3'b001, mac=3'b010, next_in=3'b011, store=3'b100, next_out=3'b101, done=3'b110; //FSM States
   reg [2:0] cur, nxt; //State
@@ -23,9 +24,9 @@ module fc
       idle    : nxt = start ? init_out : idle;
       init_out: nxt = mac;
       mac     : nxt = next_in;
-      next_in : nxt = in_idx<5 ? mac : store;
+      next_in : nxt = in_idx<=5 ? mac : store;
       store   : nxt = next_out;
-      next_out: nxt = out_idx<3 ? init_out : done;
+      next_out: nxt = out_idx<=3 ? init_out : done;
       done    : nxt = idle;
     endcase
   end
@@ -37,10 +38,16 @@ module fc
       in_idx<=0;
       out_idx<=0;
       {output_vec[0], output_vec[1], output_vec[2], output_vec[3]} <= 128'b0;
+      for(i=0;i<6;i=i+1) input_vec[i] <= 0;
+      for(i=0;i<6;i=i+1) for(j=0;j<4;j=j+1) weights[i][j] <= 0;
     end
     else begin
       case(cur)
-        idle    : begin input_vec<=x; weights <= w; finished<=0; in_idx<=0; out_idx<=0; end
+        idle    : begin 
+          for (i=0;i<6;i=i+1) input_vec[i] <= x[i];
+          for(i=0;i<6;i=i+1) for(j=0;j<4;j=j+1) weights[i][j] <= w[i][j]; 
+          finished<=0; in_idx<=0; out_idx<=0; 
+        end
         init_out: begin acc<=0; in_idx<=0; end
         mac     : begin acc <= acc + input_vec[in_idx]*weights[in_idx][out_idx]; in_idx<=in_idx+1; end
         store   : begin output_vec[out_idx]<=acc; out_idx<=out_idx+1; end
